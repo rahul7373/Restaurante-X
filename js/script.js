26b0f6ad9b410c45f0f31bc349a29c1bbc6db0e7
rahul7373 <developerrahul77@gmail.com>
2022-02-18 18:45:21 +0530

    fix sum bugs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let menuBtn = document.querySelector(".menuBtn");
 let navList = document.querySelector(".nav-list");
 
-menuBtn.onclick = function(){
+menuBtn.onclick = function () {
   navList.classList.toggle('active');
   menuBtn.classList.toggle('fa-bars');
   menuBtn.classList.toggle('fa-times');
@@ -11,17 +11,18 @@ menuBtn.onclick = function(){
 
 // swipper js
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: false,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  grabCursor: true,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
